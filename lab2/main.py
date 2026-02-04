9a2afa33687f176d27f015d47bdcef2836ae9651
Jammedat <susilkhadka503@gmail.com>
2026-02-04 23:50:36 +0545

    changed the path

--- lab2/main.py
+++ lab2/main.py
@@ -1,11 +1,16 @@
 import streamlit as st
 import joblib
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_PATH = os.path.join(BASE_DIR, "nepali_news_classifier.joblib")
+ENCODER_PATH = os.path.join(BASE_DIR, "nepali_news_label_encoder.joblib")
 
 st.title("News category prediction")
 input_text = st.text_input("Enter the news you want to predict")
 
-model = joblib.load("nepali_news_classifier.joblib")
-label_encoder = joblib.load("nepali_news_label_encoder.joblib")  # load encoder
+model = joblib.load(MODEL_PATH)
+label_encoder = joblib.load(ENCODER_PATH)  # load encoder
 
 if st.button("PREDICT"):
     output = model.predict([input_text])
